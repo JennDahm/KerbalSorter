4e39ad3f7f13bde1fcadbdcbb5d20a19568b5224
Jon Dahm <jdahm@utexas.edu>
2015-06-09 00:37:55 -0500

    Added hooks for the Editor. TODO: Add fly-in animation to match the Crew tab fly-in animation.

---
+++ Hooks/EditorHook.cs
@@ -0,0 +1,71 @@
+﻿using KSP;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace KerbalSorter.Hooks
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    class EditorHook : MonoBehaviour {
+        SortingButtons sortBar;
+        
+        protected void Start() {
+            try{
+                GameEvents.onEditorScreenChange.Add(OnEditorScreenChange);
+
+                // Get Roster:
+                UIScrollList[] lists = UIManager.instance.gameObject.GetComponentsInChildren<UIScrollList>(true);
+                UIScrollList availableCrew = null;
+                foreach (UIScrollList list in lists)
+                {
+                    if (list.name == "scrolllist_avail")
+                    {
+                        availableCrew = list;
+                        break;
+                    }
+                }
+                if (availableCrew == null)
+                {
+                    throw new Exception("Could not find Available Crew List!");
+                }
+                //Roster available = ...
+
+
+                // Set position: (This is probably the one time we actually want to do this here)
+                Transform tab_crewavail = availableCrew.transform.parent.Find("tab_crewavail");
+                BTButton tab = tab_crewavail.GetComponent<BTButton>();
+                Vector3 tabPos = Utilities.GetPosition(tab_crewavail);
+                float x = tabPos.x + tab.width + 5;
+                float y = tabPos.y - 1;
+
+                // Set up button list:
+                SortButtonDef[] buttons = new SortButtonDef[]{ StandardButtonDefs.ByName,
+                    StandardButtonDefs.ByClass, StandardButtonDefs.ByLevel, StandardButtonDefs.ByGender
+                };
+
+                // Initialize the sort bar:
+                sortBar = gameObject.AddComponent<SortingButtons>();
+                //sortBar.SetRoster(available);
+                sortBar.SetButtons(buttons);
+                sortBar.SetPos(x, y);
+                sortBar.enabled = false;
+            }
+            catch (Exception e){
+                Debug.LogError("KerbalSorter: Unexpected error in Editor Hook! " + e);
+            }
+        }
+
+        protected void OnDestroy() {
+            GameEvents.onEditorScreenChange.Remove(OnEditorScreenChange);
+        }
+
+        protected void OnEditorScreenChange(EditorScreen screen) {
+            if( screen == EditorScreen.Crew ){
+                sortBar.enabled = true;
+            } else {
+                sortBar.enabled = false;
+            }
+        }
+    }
+}
