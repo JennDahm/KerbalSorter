7e4b1bb0c7b6de77359cdaf9d92a08d3d59c609d
Jon Dahm <jdahm@utexas.edu>
2015-06-20 02:52:50 -0500

    Added fly-in animation to the sort bar in the Editor.

--- Hooks/EditorHook.cs
+++ Hooks/EditorHook.cs
@@ -49,6 +49,19 @@ namespace KerbalSorter.Hooks
                 sortBar.SetDefaultOrdering(StandardKerbalComparers.DefaultAvailable);
                 sortBar.SetPos(x, y);
                 sortBar.enabled = false;
+
+                // Create a fly-in animation for the sort bar.
+                baseX = x;
+                baseY = y;
+                float animBeginTime = 0.2f;
+                animEndTime = 0.5f;
+                anim = AnimationCurve.Linear(animBeginTime, -575f, animEndTime, 0f);
+
+                // This is what I would have *liked* to have done, but Unity decided this should do absolutely nothing.
+                /*AnimationCurve curve = AnimationCurve.Linear(0f, 0f, 10f, 100f);
+                AnimationClip clip = new AnimationClip();
+                clip.SetCurve("", typeof(Transform), "position.x", curve);
+                sortBar.gameObject.AddComponent<Animation>().AddClip(clip, "flyin");*/
             }
             catch (Exception e){
                 Debug.LogError("KerbalSorter: Unexpected error in Editor Hook! " + e);
@@ -62,8 +75,36 @@ namespace KerbalSorter.Hooks
         protected void OnEditorScreenChange(EditorScreen screen) {
             if( screen == EditorScreen.Crew ){
                 sortBar.enabled = true;
+                playAnimation = true;
+                animProgress = 0f;
+                // To ensure a smooth transition:
+                sortBar.SetPos(baseX + anim.Evaluate(0f), baseY);
+
+                //sortBar.gameObject.GetComponent<Animation>().Play("flyin");
             } else {
                 sortBar.enabled = false;
+            }
+        }
+
+
+        // ------- Animation handling -------
+        // Unity doesn't want to work with us on
+        // animation, so we have to do it manually.
+        AnimationCurve anim;
+        bool playAnimation = false;
+        float animProgress = 0f;
+        float animEndTime;
+        float baseX;
+        float baseY;
+
+        protected void Update() {
+            if( playAnimation ){
+                if( animProgress > animEndTime ){
+                    playAnimation = false;
+                }
+                float x = baseX + anim.Evaluate(animProgress);
+                sortBar.SetPos(x, baseY);
+                animProgress += Time.deltaTime;
             }
         }
     }
