fba5b9d2e10ca5c5009fd52082e28a65654bb640
Jon Dahm <jdahm@utexas.edu>
2015-06-19 20:50:17 -0500

    Added tab-switch handling to Astronaut Complex. Added crew hire handling to Astronaut Complex.

--- Hooks/AstronautComplexHook.cs
+++ Hooks/AstronautComplexHook.cs
@@ -10,23 +10,28 @@ namespace KerbalSorter.Hooks
     public class AstronautComplexHook : MonoBehaviour {
         CMAstronautComplex complex;
         SortingButtons sortBar;
-        Roster<IUIListObject> available;
-        Roster<IUIListObject> assigned;
-        Roster<IUIListObject> killed;
+        StockRoster available;
+        StockRoster assigned;
+        StockRoster killed;
+        CrewPanel curPanel;
 
         protected void Start() {
             try{
                 // Set up hooks:
                 GameEvents.onGUIAstronautComplexSpawn.Add(OnACSpawn);
                 GameEvents.onGUIAstronautComplexDespawn.Add(OnACDespawn);
+                GameEvents.OnCrewmemberHired.Add(OnHire);
+                //GameEvents.OnCrewmemberSacked.Add(OnFire);
 
                 // Get rosters:
                 complex = UIManager.instance.gameObject.GetComponentsInChildren<CMAstronautComplex>(true).FirstOrDefault();
                 if (complex == null) throw new Exception("Could not find astronaut complex");
                 UIScrollList availableCrew = complex.transform.Find("CrewPanels/panel_enlisted/panelManager/panel_available/scrolllist_available").GetComponent<UIScrollList>();
-                //UIScrollList assignedCrew = complex.transform.Find("CrewPanels/panel_enlisted/panelManager/panel_assigned/scrolllist_assigned").GetComponent<UIScrollList>();
-                //UIScrollList killedCrew = complex.transform.Find("CrewPanels/panel_enlisted/panelManager/panel_kia/scrolllist_kia").GetComponent<UIScrollList>();
+                UIScrollList assignedCrew = complex.transform.Find("CrewPanels/panel_enlisted/panelManager/panel_assigned/scrolllist_assigned").GetComponent<UIScrollList>();
+                UIScrollList killedCrew = complex.transform.Find("CrewPanels/panel_enlisted/panelManager/panel_kia/scrolllist_kia").GetComponent<UIScrollList>();
                 available = new StockRoster(availableCrew);
+                assigned = new StockRoster(assignedCrew);
+                killed = new StockRoster(killedCrew);
 
                 // Set up button list:
                 SortButtonDef[] buttons = new SortButtonDef[]{ StandardButtonDefs.ByName,
@@ -38,6 +43,24 @@ namespace KerbalSorter.Hooks
                 sortBar.SetRoster(available);
                 sortBar.SetButtons(buttons);
                 sortBar.enabled = false;
+                curPanel = CrewPanel.Available;
+
+
+                // Assign enable listeners to the rosters:
+                EnableListener listener = availableCrew.gameObject.AddComponent<EnableListener>();
+                listener.Panel = CrewPanel.Available;
+                listener.Callback = OnTabSwitch;
+                listener.SkipFirstTime = true;
+                
+                listener = assignedCrew.gameObject.AddComponent<EnableListener>();
+                listener.Panel = CrewPanel.Assigned;
+                listener.Callback = OnTabSwitch;
+                listener.SkipFirstTime = true;
+                
+                listener = killedCrew.gameObject.AddComponent<EnableListener>();
+                listener.Panel = CrewPanel.Killed;
+                listener.Callback = OnTabSwitch;
+                listener.SkipFirstTime = true;
             } catch( Exception e ){
                 Debug.Log("KerbalSorter: Unexpected error in AstronautComplex Hook: " + e);
             }
@@ -52,6 +75,7 @@ namespace KerbalSorter.Hooks
             float x = screenPos.x+targetTab.width+5;
             float y = screenPos.y - 1;
             sortBar.SetPos(x,y);
+            OnTabSwitch(CrewPanel.Available);
 
             sortBar.enabled = true;
         }
@@ -60,9 +84,56 @@ namespace KerbalSorter.Hooks
             sortBar.enabled = false;
         }
 
+        protected void OnHire(ProtoCrewMember kerbal, int numActiveKerbals) {
+            sortBar.SortRoster(true);
+        }
+
+        /*protected void OnFire(ProtoCrewMember kerbal, int numActiveKerbals) {
+            Debug.Log("KerbalSorter: OnFire called: "+ kerbal.name + " - " + something);
+        }*/
+
+        protected void OnTabSwitch(CrewPanel panel){
+            if( this.curPanel == panel ){
+                return;
+            }
+            this.curPanel = panel;
+
+            StockRoster roster = null;
+            switch( panel ){
+                case CrewPanel.Available: roster = this.available; break;
+                case CrewPanel.Assigned:  roster = this.assigned; break;
+                case CrewPanel.Killed:    roster = this.killed; break;
+            }
+            sortBar.SetRoster(roster);
+        }
+
         protected void OnDestroy() {
             GameEvents.onGUIAstronautComplexSpawn.Remove(OnACSpawn);
             GameEvents.onGUIAstronautComplexDespawn.Remove(OnACDespawn);
+            GameEvents.OnCrewmemberHired.Remove(OnHire);
+            //GameEvents.OnCrewmemberSacked.Remove(OnFire);
+        }
+
+
+        protected enum CrewPanel {
+            Available,
+            Assigned,
+            Killed
+        }
+
+        // Use: Assign as component to component you want to listen to.
+        protected class EnableListener : MonoBehaviour {
+            public CrewPanel Panel;
+            public Action<CrewPanel> Callback;
+            public bool SkipFirstTime = false;
+            private bool _doneFirstTime = false;
+            protected void OnEnable(){
+                if( SkipFirstTime && !_doneFirstTime ){
+                    _doneFirstTime = true;
+                    return;
+                }
+                Callback(Panel);
+            }
         }
     }
 
